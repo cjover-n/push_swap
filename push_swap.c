a388c48baffe08675e5fba7d0624decf8e3c8b4b
Cristina Jover navarro <cjover-n@c1r14s5.42madrid.com>
2021-05-25 22:30:01 +0200

    list

--- push_swap.c
+++ push_swap.c
@@ -6,30 +6,45 @@
 /*   By: cjover-n <cjover-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 21:54:18 by cjover-n          #+#    #+#             */
-/*   Updated: 2021/05/20 21:24:53 by cjover-n         ###   ########.fr       */
+/*   Updated: 2021/05/25 22:28:37 by cjover-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int		ft_isnum(char *str)
+{
+	int	count;
+
+	count = 0;
+	if ((str[count] == '+' || str[count] == '-') && ft_strlen(str) > 1)
+		count++;
+	while (str[count] != '\0')
+	{
+		if (ft_isdigit(str[count]) == 0)
+			return (0);
+		count++;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list		*a;
 	t_list		*b;
-	
-	int	num;
+	int i;
 
 	if (argc < 2)
 		return (1);
-	ft_bzero(&p, sizeof(p));
-	while (--argc > 0)
+	char **arr = ft_split(argv[1], ' ');
+	i = 0;
+	b = NULL;
+	while (arr[i] != 0)
 	{
-		if (!ft_isdigit(ft_atoi(argv[argc])))
-			return (0);
-		num  = ft_atoi(argv[argc]);
-		if (num < INT_MIN || num > INT_MAX)
-			return (0);
+		a = ft_lstnew(arr[i]);
+		if (!ft_isnum(a->content))
+			return(0);
+		printf("[%s]\n", a->content);
+		i++;
 	}
-	while(num)
-		printf("%i\n", num);
 }
